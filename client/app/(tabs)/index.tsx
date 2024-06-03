548c9122196ce215d2faa7c73caafd92e78a11c5
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-06-03 20:00:43 +0300

    Added SearchBooks Query

--- client/app/(tabs)/index.tsx
+++ client/app/(tabs)/index.tsx
@@ -1,11 +1,54 @@
-import { StyleSheet } from "react-native";
+import { ActivityIndicator, StyleSheet } from "react-native";
 
 import { Text, View } from "@/components/Themed";
+import { gql, useQuery } from "@apollo/client";
 
+const query = gql`
+  query SearchBooks($q: String) {
+    googleBooksSearch(q: $q, country: "US") {
+      items {
+        id
+        volumeInfo {
+          authors
+          averageRating
+          description
+          imageLinks {
+            thumbnail
+          }
+          title
+          subtitle
+          industryIdentifiers {
+            identifier
+            type
+          }
+        }
+      }
+    }
+    openLibrarySearch(q: $q) {
+      docs {
+        author_name
+        title
+        cover_edition_key
+        isbn
+      }
+    }
+  }
+`;
 export default function TabOneScreen() {
+  const { data, loading, error } = useQuery(query, {
+    variables: { q: "React Native" },
+  });
+
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Books App</Text>
+      {loading && <ActivityIndicator />}
+      {error && (
+        <View style={styles.container}>
+          <Text style={styles.title}>Error fetching books</Text>
+          <Text>{error.message}</Text>
+        </View>
+      )}
     </View>
   );
 }
